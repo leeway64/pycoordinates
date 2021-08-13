00d9d9f395684385a1d8b00589f12ff79fa117a4
Lee-Way Wang <lee.way56@comcast.net>
2021-08-12 18:54:33 -0700

    Initial commit
    - Create __init__.py and setup.py
    - Create gitignore file and README

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(  name='pycoordinates',
+        version='1.0',
+        description='A Python package that tells you information about the user-specified\
+                    latitude and longitude',
+        url='',
+        author='leeway64',
+        packages=find_packages(),
+        zip_safe=False)
