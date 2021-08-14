0c9a3d90fd1589ccc6d9de78482aeb18ad709392
Lee-Way Wang <lee.way56@comcast.net>
2021-08-13 19:14:39 -0700

    Make minor edits to arguments

--- pycoordinates/pycoordinates.py
+++ pycoordinates/pycoordinates.py
@@ -2,11 +2,15 @@ import argparse
 
 
 def parse_commands():
-    parser = argparse.ArgumentParser(description="Provides information on give coordinates")
+    parser = argparse.ArgumentParser(prog='pycoordinates', description="Provides information \
+                                on given coordinates and returns coordinates of given location")
+    parser.version = 1.0
+
     parser.add_argument('--coordinates', action='store', nargs=2, help='Latitude and longitude')
     parser.add_argument('--location', action='store', nargs=1, help='Name of the location')
-    parser.add_argument('--cardinal', action='store_true', nargs=1, help='Allows for cardinal\
-    coordinates to be typed (e.g., 25N, 121E)')
+    parser.add_argument('--cardinal', action='store_true', help='Allows for cardinal coordinates\
+                                                                to be typed (e.g., 25N, 121E)')
+    parser.add_argument('--version', action='version')
 
 if __name__ == '__main__':
     parse_commands()
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(  name='pycoordinates',
         version='1.0',
         description='A Python package that tells you the location of the given coordinates, or\
-            gives the coordinates to a given location.',
+                    gives the coordinates to a given location.',
         url='https://github.com/leeway64/pycoordinates',
         author='leeway64',
         packages=find_packages(),
