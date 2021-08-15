9d16346d62dad343a10d2320953a8e81d9de1477
Lee-Way Wang <lee.way56@comcast.net>
2021-08-15 10:45:24 -0700

    test directory, gitignore, setup, pycoordinates
    - Create directory for tests
    - Update gitignore and setup
    - In pycoordinates, create get_location and get_coordinates functions

--- pycoordinates/pycoordinates.py
+++ pycoordinates/pycoordinates.py
@@ -1,4 +1,20 @@
 import argparse
+import time
+from geopy.geocoders import Nominatim
+
+app = Nominatim(user_agent="tutorial")
+
+
+def get_location(latitude, longitude):
+    coordinates = f"{latitude}, {longitude}"
+    # Nominatim only allows 1 request per second
+    time.sleep(1)
+    location = app.reverse(coordinates).raw
+    
+
+
+def get_coordinates(location):
+    pass
 
 
 def parse_commands():
@@ -11,6 +27,19 @@ def parse_commands():
     parser.add_argument('--cardinal', action='store_true', help='Allows for cardinal coordinates\
                                                                 to be typed (e.g., 25N, 121E)')
     parser.add_argument('--version', action='version')
+
+    arguments = parser.parse_args()
+
+    if arguments.coordinates:
+        pass
+    elif arguments.coordinates and arguments.cardinal:
+        pass
+
+    if arguments.location:
+        pass
+    elif arguments.location and arguments.cardinal:
+        pass
+
 
 if __name__ == '__main__':
     parse_commands()
--- setup.py
+++ setup.py
@@ -4,7 +4,9 @@ setup(  name='pycoordinates',
         version='1.0',
         description='A Python package that tells you the location of the given coordinates, or\
                     gives the coordinates to a given location.',
+        long_description=open('README.rst').read(),
         url='https://github.com/leeway64/pycoordinates',
         author='leeway64',
         packages=find_packages(),
-        zip_safe=False)
+        zip_safe=False,
+        install_requires='geopy')
