8ed4a9e4781249c575c4143acec28f6c1cb03a5d
Lee-Way Wang <lee.way56@comcast.net>
2021-08-20 19:38:15 -0700

    Reformat get_*, create test harness
    - Reformat get_location and get_coordinates to account for verbose option
    - Create test harness for pytest to run
    - Also remove requirements.txt

--- pycoordinates/pycoordinates.py
+++ pycoordinates/pycoordinates.py
@@ -5,28 +5,44 @@ from geopy.geocoders import Nominatim
 app = Nominatim(user_agent="tutorial")
 
 
-def get_location(latitude, longitude):
+def get_location(latitude, longitude, verbose):
     coordinates = f"{latitude}, {longitude}"
     location_info = app.reverse(coordinates).raw
-    
-    print(f'Country: {location_info["country"]}')
 
-    if location_info['country'] == 'United States':
-        print(f'State: {location_info["state"]}')
-    
-    print(f'City: {location_info["city"]}')
+    location = ''
+
+    if verbose:
+        print(f'City: {location_info["address"]["city"]}')
+        
+        if location_info["address"]['country'] == 'United States':
+            print(f'State: {location_info["address"]["state"]}')
+        
+        print(f'Country: {location_info["address"]["country"]}')
+    else:
+        print(f'{location_info["address"]["city"]}, ', end='')
+        
+        if location_info["address"]['country'] == 'United States':
+            print(f'{location_info["address"]["state"]}, ', end='')
+        
+        print(f'{location_info["address"]["country"]}')
 
     # Nominatim only allows 1 request per second
     time.sleep(1)
 
 
-def get_coordinates(location):
+def get_coordinates(location, verbose):
     location_info = app.geocode(location).raw
     latitude = float(location_info["lat"])
     longitude = float(location_info["lon"])
-    print(f'Location: {location}')
-    print(f'Latitude: {latitude:.2f}')
-    print(f'Longitude: {longitude:.2f}')
+    
+    coordinates = ''
+
+    if verbose:
+        print(f'Location: {location}')
+        print(f'Latitude: {latitude:.2f}')
+        print(f'Longitude: {longitude:.2f}')
+    else:
+        print(f'{location}: {latitude:.2f}, {longitude:.2f}')
 
     # Nominatim only allows 1 request per second
     time.sleep(1)
@@ -39,9 +55,7 @@ def parse_commands():
 
     parser.add_argument('--coordinates', action='store', nargs=2, help='Latitude and longitude')
     parser.add_argument('--location', action='store', nargs=1, help='Name of the location')
-    parser.add_argument('--cardinal', action='store_true', help='Allows for cardinal coordinates\
-                                                                to be typed (e.g., 25N, 121E)')
-    parser.add_argument('--verbose', action='store_true')
+    parser.add_argument('--verbose', action='store_true', help="Produces a more verbose output")
     parser.add_argument('--version', action='version')
 
 
@@ -50,15 +64,10 @@ def parse_commands():
     if arguments.coordinates:
         latitude = arguments.coordinates[0]
         longitude = arguments.coordinates[1]
-        get_location(latitude, longitude)
-    elif arguments.coordinates and arguments.cardinal:
-        pass
+        get_location(latitude, longitude, arguments.verbose)
 
     if arguments.location:
-        get_coordinates(arguments.location)
-    elif arguments.location and arguments.cardinal:
-        pass
-
+        get_coordinates(arguments.location, arguments.verbose)
 
 if __name__ == '__main__':
     parse_commands()
---
+++ tests/tests_pycoordinates.py
@@ -0,0 +1,6 @@
+class TestHarness:
+    def test_get_location():
+        pass
+
+    def test_get_coordinates():
+        pass
