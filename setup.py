cb564ef8d64731ec25e4dd1e35e4d5914f58c073
Lee-Way Wang <lee.way56@comcast.net>
2021-08-22 17:43:46 -0700

    Change version to 0.1

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from pycoordinates.test.test_pycoordinates import TestSuite
 from setuptools import setup, find_packages
+# import 
 
 setup(  name='pycoordinates',
-        version='1.0',
+        version= '0.1',
         description='A Python package that gives the location of the given coordinates, or\
                     gives the coordinates to a given location.',
         long_description=open('README.rst').read(),
